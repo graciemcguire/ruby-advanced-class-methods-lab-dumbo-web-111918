9f4678d0a2489903cd16521885a4b255e507d625
Gracie McGuire <m.graciemcguire@gmail.com>
2018-11-25 17:34:56 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,14 +3,14 @@ class Song
   
   @@all = []
   
+  def self.all
+    @@all
+  end
+  
   def save
     self.class.all << self
   end
   
-  def self.all
-    @@all
-  end
-
   def self.create
     new_song = self.new 
     new_song.save 
